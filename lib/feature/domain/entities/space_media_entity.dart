2085400399a15c23ca56d3b5a1d299d447d9ab80
Ariana Camargos <arianacamargos16@gmail.com>
2023-02-28 21:01:05 -0300

    feat: insere copyWith na entidade

--- lib/feature/domain/entities/space_media_entity.dart
+++ lib/feature/domain/entities/space_media_entity.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class SpaceMediaEntity extends Equatable {
@@ -12,13 +13,26 @@ class SpaceMediaEntity extends Equatable {
     required this.title,
     required this.mediaUrl,
   });
+
   @override
-  List<Object?> get props => [
+  List<Object> get props => [
         description,
         mediaType,
         title,
         mediaUrl,
       ];
 
-      
+  SpaceMediaEntity copyWith({
+    String? description,
+    String? mediaType,
+    String? title,
+    String? mediaUrl,
+  }) {
+    return SpaceMediaEntity(
+      description: description ?? this.description,
+      mediaType: mediaType ?? this.mediaType,
+      title: title ?? this.title,
+      mediaUrl: mediaUrl ?? this.mediaUrl,
+    );
+  }
 }
